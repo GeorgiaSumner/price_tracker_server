f013f008649b2b6d475232f1658fc07c0e3ee8e5
Georgia Sumner <georgiasumner@developersinstitute.co.nz>
2023-02-19 01:18:23 +1300

    attempting to resolve error

--- server.js
+++ server.js
@@ -22,10 +22,6 @@ app.post("/cardSearch", (req, res) => {
     res.send(data);
   });
 
-  childPython.stderr.on("data", (data) => {
-    console.error(`stderr: ${data}`);
-  });
-
   childPython.on("close", (code) => {
     console.log(`child process exited with code ${code}`);
   });
