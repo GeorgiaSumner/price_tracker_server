4649e65c8e1b467cfd859c524daec1e623c14fca
Georgia Sumner <georgiasumner@developersinstitute.co.nz>
2023-02-18 23:40:07 +1300

    changed port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
